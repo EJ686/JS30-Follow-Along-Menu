ce722af07365a3415a7f0170905602e03c08c823
Edgar Juarez <ejjt.686@gmail.com>
2017-06-22 18:43:10 -0700

    Added trigger class when mouse enters

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
 
 function handleEnter() {
+    this.classList.add('trigger-enter');
+    setTimeout(() => this.classList.add('trigger-enter-active'), 150);
     console.log('Enter');
 }
 
